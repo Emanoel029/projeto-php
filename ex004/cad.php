b788d50a9c4c5f53b1fef66fd0491adcaa74796d
Emanoel Timbo <138140487+Emanoel029@users.noreply.github.com>
2023-10-21 12:35:28 -0300

    Tela simples de cadastro em PHP

---
+++ ex004/cad.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Resultado</title>
+</head>
+
+<body>
+  <header>
+    <h1>Resultado do processamento</h1>
+  </header>
+  <main>
+    <?php
+
+    $nome = $_GET["nome"] ?? "sem nome";
+    $sbrenome = $_GET["sobrenome"] ?? "Desconecido";
+
+    echo "É um prazer te conhecer, <strong> $nome $sbrenome!</strong> Este é o meu site."
+
+    ?>
+    <p><a href="javascript:history.go(-1)">Voltar</a></p>
+  </main>
+
+</body>
+
+</html>
